d5eb8f79eee093857e7417a465119731bce6d3cb
Tikhon Rulkevich <tnxah.j@gmail.com>
2023-06-19 12:48:13 +0200

    Init

---
+++ Assets/Unit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Fiendly Unit", menuName = "Fiendly Unit")]
+public class Unit : ScriptableObject
+{
+    public new string name;
+
+    public int manaCost;
+    public int attack;
+    public int attackSpeed;
+    public int moveSpeed;
+    public int health;
+    public float cooldown;
+}
